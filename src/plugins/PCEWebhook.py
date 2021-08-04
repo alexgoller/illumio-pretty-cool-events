7b1495518a3083482d2b54dae12758c1b304928f
Alex Goller <alex@vivien.franken.de>
2021-08-04 21:34:52 +0000

    More webhook code

--- src/plugins/PCEWebhook.py
+++ src/plugins/PCEWebhook.py
@@ -1,7 +1,17 @@
+import requests
 import json
 
 class PCEWebhook(OutputPlugin):
 
     def config(self,config):
+        if 'bearer_token' in config:
+            self.bearer_token = config['bearer_token']
+        if 'url' in config:
+            self.url = config['url']
+        if 'data' in config:
+            self.data = config['data']
 
     def output(self,output):
+        data = output
+        r = requests.post(self.url, data=json.dumps(data), 
+                          headers={'Content-Type': 'application/json'})
