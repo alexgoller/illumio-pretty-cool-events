e9db0937ddddd3c4b1c5e0aadb3402348135b27d
Alex Goller <alex@vivien.franken.de>
2021-08-21 20:43:17 +0000

    Add jinja2 to init phase, FileSystemLoader

--- src/outputplugin.py
+++ src/outputplugin.py
@@ -1,8 +1,9 @@
-from jinja2 import Template
+from jinja2 import Environment, FileSystemLoader, select_autoescape, Template
 
 class OutputPlugin:
     def __init__(self):
         print("Do the init thing")
+        self.env = Environment(loader=FileSystemLoader('../templates'), autoescape=select_autoescape(['html', 'xml']))
 
     def output(self, output):
         print("Output something")
