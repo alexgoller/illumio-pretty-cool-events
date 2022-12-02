a5dbf6d920b74c01401e36ae850f9263ae3c4245
alexgoller <83632450+alexgoller@users.noreply.github.com>
2022-12-02 12:48:35 +0100

    Update pagerduty plugin for added priority and service IDs

--- src/plugins/PCEPagerDuty.py
+++ src/plugins/PCEPagerDuty.py
@@ -15,6 +15,15 @@ class PCEPagerDuty(OutputPlugin):
 
         if 'template' in config:
             self.template = config['template']
+
+        if 'pd_from' in config:
+            self.pd_from = config['pd_from']
+            
+        if 'pd_priority' in config:
+            self.pd_from = config['pd_priority']
+
+        if 'pd_service' in config:
+            self.pd_from = config['pd_service']
     
     def output(self, output, extra_data, template_globals):
         logging.debug("PCEPagerDuty: data: {}".format(output))
@@ -31,6 +40,6 @@ class PCEPagerDuty(OutputPlugin):
         rtemplate = self.env.get_template(template)
         template_output = rtemplate.render(output)
 
-        session.rpost('/incidents', json={ 'incident': { 'type': 'incident', 'title': 'PCE Alert', 'priority': { 'id': 'P7I8BQT', 'type': 'priority_reference' }, 'service': { 'id': 'P1535BG', 'type': 'service_reference' }, 'body': { 'type': 'incident_body', 'details': template_output }}} )
+        session.rpost('/incidents', json={ 'incident': { 'type': 'incident', 'title': 'PCE Alert', 'priority': { 'id': self.pd_priority, 'type': 'priority_reference' }, 'service': { 'id': self.pd_service, 'type': 'service_reference' }, 'body': { 'type': 'incident_body', 'details': template_output }}} )
 
         logging.debug("PCEPagerDuty: output: {}".format(template_output))
