fbb7478e572c3859d8ed49c397c674d346e722c5
alexgoller <83632450+alexgoller@users.noreply.github.com>
2022-12-07 10:30:52 +0100

    Entry.sh updated

--- entry.sh
+++ entry.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
+export PYTHONWARNINGS="ignore:Unverified HTTPS request"
 cd /pretty-cool-events/src
 python3 /pretty-cool-events/src/pretty-cool-events --config /pretty-cool-events/config.yaml
